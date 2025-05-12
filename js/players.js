21318d89841e2b8466e4d9f2a268ce3bef17613f
kenzelsherbini <526kelsherbini@frhsd.com>
2025-05-12 10:44:26 -0400

    first commit

---
+++ js/players.js
@@ -0,0 +1,37 @@
+const players = [
+    {
+        firstName: "Lionel",
+        lastName: "Messi",
+        position: 'SS',
+        age: 36,
+        photo: "https://upload.wikimedia.org/wikipedia/commons/0/0c/Lionel_Messi_20180626.jpg",
+    },
+    {
+        firstName: "Lionel",
+        lastName: "Messi",
+        position: 'SS',
+        age: 36,
+        photo: "https://upload.wikimedia.org/wikipedia/commons/0/0c/Lionel_Messi_20180626.jpg",
+    },
+    {
+        firstName: "Lionel",
+        lastName: "Messi",
+        position: 'SS',
+        age: 36,
+        photo: "https://upload.wikimedia.org/wikipedia/commons/0/0c/Lionel_Messi_20180626.jpg",
+    },
+    {
+        firstName: "Lionel",
+        lastName: "Messi",
+        position: 'SS',
+        age: 36,
+        photo: "https://upload.wikimedia.org/wikipedia/commons/0/0c/Lionel_Messi_20180626.jpg",
+    },
+    {
+        firstName: "Lionel",
+        lastName: "Messi",
+        position: 'SS',
+        age: 36,
+        photo: "https://upload.wikimedia.org/wikipedia/commons/0/0c/Lionel_Messi_20180626.jpg",
+    }
+]
