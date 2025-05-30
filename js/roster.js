3ccfa0417ee36477ada1d95c4300ace819cdfcfe
kenzelsherbini <526kelsherbini@frhsd.com>
2025-05-30 09:27:28 -0400

    updatec card style

--- js/roster.js
+++ js/roster.js
@@ -8,13 +8,16 @@ document.addEventListener("DOMContentLoaded", ()=> {
             const col = document.createElement('div')
             col.className = 'col-6 col-lg-2'
             col.innerHTML = `
-
-               <div class="card-body text-center">
-                 <img src="${p.photo}" alt="" class="card-img-top">
-                    <h5 class="card-title mb-1">${p.firstName} ${p.lastName}</h5>
-                    <div class='badge badge-position badge-pos-${p.position}'>${p.position}</div>
+                    <div class="card card-body text-center" style="width: 18rem;">
+  <img src"${p.photo}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title mb-1">${p.firstName} ${p.lastName}</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+    <div class='badge badge-position badge-pos-${p.position}'>${p.position}</div>
                     <p class="small text-muted mb-0"> Age ${p.age}</p>
-                    </div>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
             `
             grid.appendChild(col)
         })
